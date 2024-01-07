39a2bbccec524d34b8181c56de4618260b27b3d4
neemasanthosh <142249280+neemasanthosh@users.noreply.github.com>
2024-01-08 01:08:11 +0530

    Add files via upload

---
+++ employee/src/main/java/com/greatlearning/employee/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.greatlearning.employee.service;
+
+import java.util.List;
+import com.greatlearning.employee.data.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> getAllEmployees();
+
+	void addNewEmployee(Employee employee);
+
+	void updateEmployee(Employee employee);
+
+	void deleteEmployeeById(int empId);
+
+	Employee getEmployeeById(int empId);
+
+}
